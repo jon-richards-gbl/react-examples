40f4a50c9f3e183d16c9427d3a9dbccad3771257
Jon Richards <jonathan.richards@uk.globallogic.com>
2023-03-27 11:04:17 +0100

    setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { configDefaults, defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.js",
+    coverage: {
+      provider: "c8",
+
+      // collect coverage for all except the files specified
+      all: true,
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        "src/assets/**",
+        "src/main/*.tsx",
+        "src/**/types/*.ts"
+      ],
+    },
+  },
 });
